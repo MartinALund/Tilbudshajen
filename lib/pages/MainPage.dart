34e1a6584c582ee2cd6b46a20e7a15c7adf37edb
Martin Lund <privatmartinlund@gmail.com>
2020-06-14 18:44:04 +0200

    Better sizing

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -20,13 +20,10 @@ class _MainPageState extends State<MainPage> {
                   fontWeight: FontWeight.bold,
                 )),
           )),
-      SizedBox(
-        height: 32,
-      ),
       Image.network(
           'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/26653649-0c7d-4840-ab49-9c8e1d7666e1/d7vzmap-2cc6a581-14a2-4473-b135-bf3359502b27.png/v1/fill/w_1192,h_670,strp/day_18_shopping_shark__1_hour__by_omutant2_d7vzmap-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDgwIiwicGF0aCI6IlwvZlwvMjY2NTM2NDktMGM3ZC00ODQwLWFiNDktOWM4ZTFkNzY2NmUxXC9kN3Z6bWFwLTJjYzZhNTgxLTE0YTItNDQ3My1iMTM1LWJmMzM1OTUwMmIyNy5wbmciLCJ3aWR0aCI6Ijw9MTkyMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.lV2Ds0fp8fQ86E1D8mUwBMxU3DOvbL13bKrByIU1gK0'),
       SizedBox(
-        height: 32,
+        height: 16,
       ),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
